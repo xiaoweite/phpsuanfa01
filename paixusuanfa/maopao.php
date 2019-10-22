52ebdf11a180ac2896fce8afc1fc2cfc97fb626a
lansonlsb <lansonlsb@gmail.com>
2019-10-22 23:33:51 +0800

    更新代码

--- paixusuanfa/maopao.php
+++ paixusuanfa/maopao.php
@@ -1,7 +1,8 @@
 <?php
-    // 性能最差的冒泡排序方法
+
     header("Content-type:text/html;charset=utf-8");
-    function maopao($arr) {
+    // 性能最差的冒泡排序方法
+    function maopao1($arr) {
     $len = count($arr);  //计算数字长度
         for ($i = 1; $i < $len; $i++) {
           // 该层循环控制需要冒泡的轮数
@@ -16,13 +17,49 @@
         return $arr;
     }
 
+    // 性能最好的冒泡排序方法
+    function maopao2($arr) {
+        // 设置变量的初始值
+        $low = 0;
+        $high = count($arr) -1;
+        while ($low < $high) {
+            // 正向冒泡,找到最大值
+            for ($j = $low; $j < $high; ++$j) {
+                if ($arr[$j] > $arr[$j+1]) {
+                    $tmp = $arr[$j];
+                    $arr[$j] = $arr[$j+1];
+                    $arr[$j+1] = $tmp;
+                }
+            }
+            --$high;
+            // 放向冒泡,找到最小值
+            for ($j = $high; $j > $low; --$j) {
+                if ($arr[$j] < $arr[$j-1]) {
+                    $tmp = $arr[$j];
+                    $arr[$j] = $arr[$j-1];
+                    $arr[$j-1] = $tmp;
+                }
+            }
+            ++$low;
+        }
+        return $arr;
+    }
+
     $arr = array(121,656,8997,11,56456,778,65454,314354,1121);
     echo "排序前:";
     foreach ($arr as $k => $val) {
         echo $val.' ';
     }
-    echo "<br>排序后:";
-    $arr = maopao($arr);
+    echo "<br>方法一排序后:";
+    $arr = maopao1($arr);
     foreach ($arr as $k => $val) {
         echo $val.' ';
+
     }
+    echo "<br>方法二排序后:";
+    $arr = maopao2($arr);
+    foreach ($arr as $k => $val) {
+        echo $val.' ';
+
+    }
+
